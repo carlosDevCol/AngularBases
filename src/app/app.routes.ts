b4918a3645bd3a73b827a561935245efbfbb525f
Carlos Castro <carlosgcn.201501@gmail.com>
2026-04-30 16:51:48 -0500

    Se hace la aplicacion de un contador, por variables y por señales

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { count } from 'rxjs';
+import { CounterPage } from './pages/counter/counter';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+    {
+        path: '',
+        component: CounterPage
+    }
+];
